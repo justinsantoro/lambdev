b1ff42eb4298be44d5f898fd80ed5f5cbdcc86cb
Justin Santoro <jzsantoro14@gmail.com>
2019-02-04 17:52:27 -0500

    Add upload flag to publish() Tweak docstrings and messages

--- lambdev/aws_lambda.py
+++ lambdev/aws_lambda.py
@@ -7,21 +7,24 @@ l = core.l
 
 
 # Publish function from $Latest. If desired alias already exists, update its version. If not, create it.
-# Assumes that the latest version of code is already in $latest via the test.py script
-def publish(alias, description=''):
+# Assumes by default that the latest version of code is already in $latest via lambdev.test()
+def publish(alias, description='', upload=False):
+    if upload:
+        core.upload_package()
+
     fn = core.get_function_name()
 
     pubresponse = l.publish_version(FunctionName=fn)
 
     if core.alias_exists(l.list_aliases(FunctionName=fn), alias):
-        print('alias exists...updating version')
+        print('- Alias already exists. Updating version...')
         aliasresponse = l.update_alias(FunctionName=core.get_function_name(),
                                        Name=alias,
                                        FunctionVersion=pubresponse['Version'])
-        print('alias %s updated to version %s' % (aliasresponse['Name'],
+        print('- Alias {} updated to version {}'.format(aliasresponse['Name'],
                                                   aliasresponse['FunctionVersion']))
     else:
-        print('creating new alias: {}'.format(alias))
+        print('- Creating new alias: {}...'.format(alias))
         l.create_alias(FunctionName=fn,
                        Name=alias,
                        FunctionVersion=pubresponse['Version'],
@@ -35,7 +38,7 @@ def test(test_object):
     response = l.invoke(FunctionName=core.get_function_name(), InvocationType='RequestResponse',
                         LogType='Tail', Payload=json.dumps(test_object))
 
-    print(u'LOG:\n{}'.format(base64.b64decode(response['LogResult']).decode('utf-8')))
+    print(u'- Function log:\n{}'.format(base64.b64decode(response['LogResult']).decode('utf-8')))
 
     if 'functionError' in response:
         raise(Exception('~~FUNCTION ERROR~~ {}'.format(json.loads(response['payload'].read().decode('utf-8')))))
@@ -45,6 +48,7 @@ def test(test_object):
 
 def create_function(function_name=None, role=None, handler=None, description=None, runtime='python3.7'):
     x = core.ProjectFolder(core.workingDir)
+    print('- Creating function...')
     response = l.create_function(FunctionName=function_name,
                                  Runtime=runtime,
                                  Role=role,
@@ -57,4 +61,4 @@ def create_function(function_name=None, role=None, handler=None, description=Non
     with open(core.workingDir + '/function_name.txt', 'w') as f:
         f.write(response['FunctionArn'])
 
-    print('CREATE_FUNCTION SUCCESS')
+    print('- Function Created')
