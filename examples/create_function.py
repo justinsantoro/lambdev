3b8db07599036e8e24a16581371204fc4e640c22
Justin Santoro <jzsantoro14@gmail.com>
2019-02-04 17:54:53 -0500

    make main function accessable through import

--- examples/create_function.py
+++ examples/create_function.py
@@ -1,10 +1,15 @@
 import lambdev
 
-if __name__ == '__main__':
 
+def main():
     response = lambdev.create(function_name='my_function_name',
                               runtime='python3.7',
                               role='arn:aws:iam::123456789012:role/example_role',
                               handler='my_script.lambda_handler',
                               description='my new lambda function'
                               )
+    return response
+
+
+if __name__ == '__main__':
+    main()
